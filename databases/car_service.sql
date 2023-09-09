1637a3fa3497b365f0cbf5d95d62b217c47b5d40
SahilMunjapara <sahilmunjapara33@gmail.com>
2023-09-09 23:21:31 +0530

    init commit

--- databases/car_service.sql
+++ databases/car_service.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2023 at 09:51 AM
+-- Generation Time: Sep 09, 2023 at 04:23 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -33,6 +33,7 @@ CREATE TABLE `services` (
   `sname` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
   `serviceDesc` varchar(10000) NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -40,9 +41,31 @@ CREATE TABLE `services` (
 -- Dumping data for table `services`
 --
 
-INSERT INTO `services` (`sid`, `sname`, `image`, `serviceDesc`, `price`) VALUES
-(1, 'oil change', '/img/oilchange.jpg', 'Ensure even tire wear and improve handling with tire rotation.', 10000),
-(2, 'break repair', '/img/tireRotation.jpg', 'Ensure even tire wear and improve handling with tire rotation.', 2000);
+INSERT INTO `services` (`sid`, `sname`, `image`, `serviceDesc`, `isActive`, `price`) VALUES
+(2, 'break repair', '/img/tireRotation.jpg', 'Ensure even tire wear and improve handling with tire rotation.', 1, 7500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_request`
+--
+
+CREATE TABLE `service_request` (
+  `id` int(11) NOT NULL,
+  `oname` varchar(100) NOT NULL,
+  `contact` int(20) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `requestType` varchar(100) NOT NULL,
+  `vehicle_name` varchar(100) NOT NULL,
+  `services` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `service_request`
+--
+
+INSERT INTO `service_request` (`id`, `oname`, `contact`, `address`, `requestType`, `vehicle_name`, `services`) VALUES
+(1, 'harsh', 999999999, 'test', 'drop-off', 'hero', 'oil change');
 
 -- --------------------------------------------------------
 
@@ -63,13 +86,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `type`) VALUES
-(1, 'harsh@123', '1234', 'harsh@gmail.com', 'admin'),
+(1, 'harsh@123', '12345', 'harsh@gmail.com', 'admin'),
+(2, 'hari', '111111', 'hari@gmail.com', 'admin'),
 (17, 'sahil@123', 'qwert', 'sahil@gmail.com', 'user'),
 (19, 'meetlo', '1234', 'meet@gmail.com', 'user'),
 (24, 'harit2123', 'harit', 'harit@gamil.com', 'user'),
 (28, 'hello1232', '12345', 'harsh@gmail.com', 'user'),
-(29, 'qwer1', 'asdfg', 'hello@gmail.com', 'user'),
-(30, 'hari', '111111', 'hari@gmail.com', 'user');
+(29, 'qwer1', 'asdfg', 'hello@gmail.com', 'user');
 
 --
 -- Indexes for dumped tables
@@ -80,6 +103,12 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`, `type`) VALUES
 --
 ALTER TABLE `services`
   ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `service_request`
+--
+ALTER TABLE `service_request`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -95,7 +124,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `service_request`
+--
+ALTER TABLE `service_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
